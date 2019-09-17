33aea78cb3a8d5dc19e16e9d99d2ef0b1847621a
Oli Birgir <Obirgir@gmail.com>
2019-09-17 09:59:36 +0000

    hugmynd

--- stortverkefnidef.py
+++ stortverkefnidef.py
@@ -1,6 +1,44 @@
-def h():
-    x += 1
+#1,3             2,3     3,3
+#1,2             2,2 |    3,2
+#1,1(byrjun)     |2,1|    3,1(sigur)
 
-x = 3
-h()
-print(x)
+"""gera veggi i kringum. spilari getur ekki farið útfyrir. þá prentast út "Not a valid direction!"""
+"""prenta út valid directions. You can Travel:"""
+"""ef spilari fer N frá 1.1 á hann að fara á 1,2 o.s.frv."""
+"""þegar spilari fer á 3,1 prentast út "Victory!" og leikurinn hættir"""
+
+#búa til def move fyrir hverja hreyfingu
+#búa til breytu sem að heitir position sem segir forritinu hvar við erum.
+#hvert sem að það hreyfist uppfærist síðan positionið
+#veggir!
+#þurfum input sem að verður move. 
+#while (spilarinn er staddur á 1,1. prentast út you can travel north)
+"""def 1.1(movement):
+    print("you can travel: (N)orth)
+    if movement ="N":
+        return North1,1
+    else:
+        print("Not a valid direction!")
+        x =failure
+        return x
+
+
+if def1.1(movement) = North1,1:
+    def1,2(movement)
+
+
+
+
+def1.1
+def1.2
+def1.3
+def2.1
+def2.2
+def2.3
+def3.1
+def3.2
+def3.3
+
+    
+    
+
